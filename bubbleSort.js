36fe31ed097f27b78265d9e289d1a78ffccff7c8
Andy Wooten <andrewdwooten@icloud.com>
2017-04-28 13:57:22 -0600

    Complete bubblesort
    In hindsight, using the sort method to bubblesort, while functional,
    kind of defeated the purpose of the exercise. Rewrote method to
    loop through the array making switches until no switches are made.
    Test is currently passing.

--- bubbleSort.js
+++ bubbleSort.js
@@ -6,8 +6,18 @@
 // };
 
 function bubbleSort(data){
-  data.sort();
-  return data;
-};
+	do{
+		switched = false;
+		for(var i = 0; i < data.length -1; i++){
+			if (data[i] > data[i+1]) {
+				var previous = data[i];
+				data[i] = data[i+1];
+				data[i+1] = previous;
+				switched = true;
+            }
+        }
+    } while (switched);
+return data;
+}
 
 module.exports = bubbleSort
