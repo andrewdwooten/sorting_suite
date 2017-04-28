d6b5ed9bec435c608c982b2351703ab83be94666
Andy Wooten <andrewdwooten@icloud.com>
2017-04-28 12:24:26 -0600

    Complete bubbleSort
    Adds bubbleSort function to bubbleSort.js.
    Same uses the .sort method to alphabetize
    an array of letters. Adds test folder with
    bubbleSort-test. Test is currently passing.

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+// function bubbleSort(data){
+//   data.sort(function(a,b){
+//     return a - b;
+//   });
+//   return data;
+// };
+
+function bubbleSort(data){
+  data.sort();
+  return data;
+};
+
+module.exports = bubbleSort
---
+++ test/bubbleSort-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const bubbleSort = require('../bubbleSort')
+const expect = require('chai').expect
+
+describe('bubbleSort function', function(){
+  it('alphabetizes an array of letters', function(){
+    test = ['z','b','m','a']
+    expect(bubbleSort(test)).to.eql(['a', 'b', 'm', 'z'])
+  })
+})
